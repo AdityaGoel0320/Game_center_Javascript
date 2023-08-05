4b7611ff7f857389be3a0b843eee132a185488e2
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-08-05 16:50:49 +0530

    deafault set

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,15 +3,15 @@ import React from 'react'
 const Intro = () => {
     return (
         <div className='flex items-center justify-center flex-col gap-5'>
-            <h3 className='h3'>Welcome to </h3>
+            <h3>Welcome to </h3>
             <div className='flex items-center justify-center'>
-                <h1  className=' h1 flex items-center justify-center'>
-                    <span style = {{"color" : "red"}} >GameCenter.com</span>
+                <h1 className='flex items-center justify-center'>
+                    GameCenter.com
                 </h1>
-                <img className = "img" src="./images/logo.png" alt="" />
+                <img className='flex items-center justify-center' src="./images/logo.png" alt="" />
             </div>
 
-            <p className='p'>It is the best place to play games with your freinds</p>
+            <p>It is the best place to play games with your freinds</p>
         </div>
 
 
